64d43a9cf3cdbda3a3579ee93554ab94ba57318b
Mahesh-Abeykoon <mahesh.a@affiniti.biz>
2023-08-20 12:54:30 +0530

    Use json output

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,8 @@ const mongoose = require("mongoose");
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.use(
     session({
